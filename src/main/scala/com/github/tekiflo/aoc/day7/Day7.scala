368fb52ff807b12f3feffb932e6b132f13bee997
Florian Inchingolo <tekiflo@gmail.com>
2020-12-07 11:04:31 +0100

    day7: add part 1 solution

---
+++ src/main/scala/com/github/tekiflo/aoc/day7/Day7.scala
@@ -0,0 +1,46 @@
+package com.github.tekiflo.aoc.day7
+
+import scala.io.Source
+
+object Day7 {
+  case class Bag(name: String, children: Map[String, Int]) {
+    def canContain(bagName: String, all: Seq[Bag]): Boolean =
+      name != bagName && children.keys.exists { key =>
+        key == bagName || all.find(_.name == key).exists(_.canContain(bagName, all))
+      }
+  }
+  object Bag {
+    def apply(line: String): Bag = {
+      val qtyBagPattern = "(\\d+) (\\w+ \\w+) bags?".r
+      def children(list: String): Map[String, Int] =
+        list
+          .split(", ")
+          .map {
+            case qtyBagPattern(qty, name) => name -> qty.toInt
+          }
+          .toMap
+      line match {
+        case s"$name bags contain $list." if list == "no other bags" => Bag(name, Map.empty)
+        case s"$name bags contain $list."                            => Bag(name, children(list))
+      }
+    }
+  }
+
+  case class Bags(all: Seq[Bag]) {
+    def howManyCanContain(bagName: String): Int =
+      all.count(_.canContain(bagName, all))
+  }
+
+  def parseInput(lines: Seq[String]): Bags = Bags(lines.map(Bag(_)))
+
+  val myBag = "shiny gold"
+
+  def main(args: Array[String]): Unit = {
+    val lines = Source.fromResource("day7/input.txt").getLines().toSeq
+    val bags  = parseInput(lines)
+    val size  = bags.all.size
+
+    val resultPart1 = bags.howManyCanContain(myBag)
+    println(s"$resultPart1/$size bags can contain $myBag")
+  }
+}
