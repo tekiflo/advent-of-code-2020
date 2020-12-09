39f479b227be42838028a4a9849e1c8c148a5d61
Florian Inchingolo <tekiflo@gmail.com>
2020-12-09 09:45:26 +0100

    day1: use Longs instead of Ints for findNSumResults

--- src/test/scala/com/github/tekiflo/aoc/day1/Day1Spec.scala
+++ src/test/scala/com/github/tekiflo/aoc/day1/Day1Spec.scala
@@ -6,19 +6,19 @@ import org.scalatest.matchers.must.Matchers
 class Day1Spec extends AnyFlatSpec with Matchers {
   behavior of "Day 1"
 
-  val input  = Seq(1721, 979, 366, 299, 675, 1456)
-  val toFind = 2020
+  val input  = Seq(1721L, 979L, 366L, 299L, 675L, 1456L)
+  val toFind = 2020L
 
   it should "return the correct result for part 1 given the test input" in {
     val n              = 2
-    val expectedResult = Seq(1721, 299)
+    val expectedResult = Seq(1721L, 299L)
 
     Day1.findNSumResult(input, toFind, n) must contain theSameElementsAs expectedResult
   }
 
   it should "return the correct result for part 2 given the test input" in {
     val n              = 3
-    val expectedResult = Seq(979, 366, 675)
+    val expectedResult = Seq(979L, 366L, 675L)
 
     Day1.findNSumResult(input, toFind, n) must contain theSameElementsAs expectedResult
   }
