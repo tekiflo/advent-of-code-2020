6cc14ad24525f46577717aebd301ce60b609c091
Florian Inchingolo <tekiflo@gmail.com>
2020-12-11 14:06:48 +0100

    day11: add part 1 solution

---
+++ src/test/scala/com/github/tekiflo/aoc/day11/Day11Spec.scala
@@ -0,0 +1,26 @@
+package com.github.tekiflo.aoc.day11
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.must.Matchers
+
+class Day11Spec extends AnyFlatSpec with Matchers {
+  behavior of "Day 11"
+
+  val lines: String =
+    """L.LL.LL.LL
+      |LLLLLLL.LL
+      |L.L.L..L..
+      |LLLL.LL.LL
+      |L.LL.LL.LL
+      |L.LLLLL.LL
+      |..L.L.....
+      |LLLLLLLLLL
+      |L.LLLLLL.L
+      |L.LLLLL.LL""".stripMargin
+
+  val input: Day11.Layout = Day11.parseInput(lines.split('\n').toSeq)
+
+  it should "return the correct result for part 1 given the test input" in {
+    Day11.resultPart1(input) mustBe 37
+  }
+}
