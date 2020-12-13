fcb7c853dfc409a9726c45e544d207c28602bf45
Florian Inchingolo <tekiflo@gmail.com>
2020-12-13 14:58:04 +0100

    day12: add part 2 solution

--- src/main/scala/com/github/tekiflo/aoc/day12/Day12.scala
+++ src/main/scala/com/github/tekiflo/aoc/day12/Day12.scala
@@ -11,7 +11,7 @@ object Day12 {
     final case object West  extends Direction('W')
     val circle                            = Seq(North, East, South, West)
     def apply(c: Char): Option[Direction] = circle.find(_.c == c)
-    def move(right: Boolean, from: Direction, degrees: Int): Direction = {
+    def turn(right: Boolean, from: Direction, degrees: Int): Direction = {
       val values = if (right) circle else circle.reverse
       values((values.indexOf(from) + (degrees / 90)) % values.length)
     }
@@ -24,26 +24,50 @@ object Day12 {
         .foldLeft((East: Direction, List.empty[(Direction, Int)])) {
           case ((facing, acc), (Right(d), length))  => (facing, (d -> length) :: acc)
           case ((facing, acc), (Left('F'), length)) => (facing, (facing -> length) :: acc)
-          case ((facing, acc), (Left(c), degrees))  => (move(c == 'R', facing, degrees), acc)
+          case ((facing, acc), (Left(c), degrees))  => (turn(c == 'R', facing, degrees), acc)
         }
         ._2
         .reverse
 
-    def endCoords(): (Int, Int) = {
-      def move(x: Int, y: Int, d: Direction, nb: Int): (Int, Int) = d match {
-        case Direction.East  => (x, y + nb)
-        case Direction.West  => (x, y - nb)
-        case Direction.North => (x + nb, y)
-        case Direction.South => (x - nb, y)
-      }
+    def endCoords(): (Int, Int) =
       compute().foldLeft((0, 0)) { case ((x, y), (d, nb)) => move(x, y, d, nb) }
+
+    def move(x: Int, y: Int, d: Direction, nb: Int): (Int, Int) = d match {
+      case Direction.East  => (x + nb, y)
+      case Direction.West  => (x - nb, y)
+      case Direction.North => (x, y + nb)
+      case Direction.South => (x, y - nb)
     }
+
+    def rotate(x: Int, y: Int, right: Boolean, degrees: Int): (Int, Int) = {
+      val values    = Seq(x, y, -x, -y)
+      val direction = if (right) 1 else -1
+      def res(offset: Int) = {
+        val res   = (offset + direction * (degrees / 90)) % values.length
+        val index = if (res < 0) res + values.length else res
+        values(index)
+      }
+      (res(0), res(1))
+    }
+
+    def withWaypoint(): (Int, Int) =
+      all
+        .map { case (d, v) => (Direction(d).toRight(d), v) }
+        .foldLeft(((10, 1), (0, 0))) {
+          case (((wx, wy), (x, y)), (Right(d), length)) => (move(wx, wy, d, length), (x, y))
+          case (((wx, wy), (x, y)), (Left('F'), times)) => ((wx, wy), (x + times * wx, y + times * wy))
+          case (((wx, wy), (x, y)), (Left(c), degrees)) => (rotate(wx, wy, c == 'R', degrees), (x, y))
+        }
+        ._2
   }
 
-  def resultPart1(instructions: Instructions): Int = {
-    val (x, y) = instructions.endCoords()
+  def manhattan(coords: (Int, Int)): Int = {
+    val (x, y) = coords
     Math.abs(x) + Math.abs(y)
   }
+
+  def resultPart1(instructions: Instructions): Int = manhattan(instructions.endCoords())
+  def resultPart2(instructions: Instructions): Int = manhattan(instructions.withWaypoint())
 
   def parseInput(lines: Seq[String]): Instructions = Instructions(
     lines.map(str => (str.head, str.tail.toInt))
@@ -54,5 +78,7 @@ object Day12 {
     val input = parseInput(lines)
     val part1 = resultPart1(input)
     println(s"day 12 part 1: $part1")
+    val part2 = resultPart2(input)
+    println(s"day 12 part 2: $part2")
   }
 }
