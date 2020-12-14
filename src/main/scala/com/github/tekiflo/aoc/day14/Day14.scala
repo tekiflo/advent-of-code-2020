19d8d3db819cd2a4e779985f8055720d6b7d0516
Florian Inchingolo <tekiflo@gmail.com>
2020-12-14 10:29:53 +0100

    day14: add part 2 solution

--- src/main/scala/com/github/tekiflo/aoc/day14/Day14.scala
+++ src/main/scala/com/github/tekiflo/aoc/day14/Day14.scala
@@ -3,32 +3,56 @@ package com.github.tekiflo.aoc.day14
 import scala.io.Source
 
 object Day14 {
-  def bin2long(str: String, trueValue: Char): Long =
+  def bin2long(str: String, trueValue: Char = '1'): Long =
     java.lang.Long.parseLong(str.map(c => if (c == trueValue) '1' else '0'), 2)
 
   sealed trait Instruction
   object Instruction {
-    case class Mask(pos: Long, neg: Long) extends Instruction {
-      def apply(l: Long): Long = ~(~(l | pos) | neg)
+    case class Mask(pos: Long, neg: Long, floating: Long) extends Instruction {
+      def part1(l: Long): Long = ~(~(l | pos) | neg)
+      def part2(i: Long): Seq[Long] = {
+        val ow = i | pos
+        val fs = floating.toBinaryString
+        val res = (0 until fs.length).foldLeft(Set(ow)) { (acc, index) =>
+          val f = bin2long(fs.substring(index))
+          acc.flatMap(n => Set(n | f, n & ~f))
+        }
+        res.toSeq
+      }
     }
-    case class Save(index: Int, value: Long) extends Instruction
+    object Mask {
+      def apply(mask: String): Mask = Mask(
+        pos = bin2long(mask, '1'),
+        neg = bin2long(mask, '0'),
+        floating = bin2long(mask, 'X')
+      )
+      val empty: Mask = Mask(0L, 0L, 0L)
+    }
+    case class Save(index: Long, value: Long) extends Instruction
     def apply(str: String): Instruction = str match {
-      case s"mask = $mask"         => Mask(bin2long(mask, '1'), bin2long(mask, '0'))
-      case s"mem[$index] = $value" => Save(index.toInt, value.toLong)
+      case s"mask = $mask"         => Mask(mask)
+      case s"mem[$index] = $value" => Save(index.toLong, value.toLong)
     }
   }
 
   import Instruction._
-  type Memory = Map[Int, Long]
-  def applyAll(instructions: Seq[Instruction]): (Mask, Memory) =
-    instructions.foldLeft((Mask(0L, 0L), Map.empty[Int, Long])) {
-      case ((_, mem), newMask: Mask)         => (newMask, mem)
-      case ((mask, mem), Save(index, value)) => (mask, mem.updated(index, mask(value)))
+  type Memory = Map[Long, Long]
+
+  def run(instructions: Seq[Instruction])(saveF: (Mask, Save, Memory) => Memory): (Mask, Memory) =
+    instructions.foldLeft((Mask.empty, Map.empty[Long, Long])) {
+      case ((_, mem), newMask: Mask) => (newMask, mem)
+      case ((mask, mem), save: Save) => (mask, saveF(mask, save, mem))
     }
 
-  def resultPart1(input: Seq[Instruction]): Long = {
-    val (_, mem) = applyAll(input)
-    mem.values.sum
+  def memSummed(input: Seq[Instruction])(saveF: (Mask, Save, Memory) => Memory): Long =
+    run(input)(saveF)._2.values.sum
+
+  def resultPart1(input: Seq[Instruction]): Long = memSummed(input) {
+    case (mask, Save(index, value), mem) => mem.updated(index, mask.part1(value))
+  }
+
+  def resultPart2(input: Seq[Instruction]): Long = memSummed(input) {
+    case (mask, Save(index, value), mem) => mem ++ mask.part2(index).map(_ -> value).toMap
   }
 
   def parseInput(lines: Seq[String]): Seq[Instruction] = lines.map(Instruction(_))
@@ -38,5 +62,7 @@ object Day14 {
     val input = parseInput(lines)
     val part1 = resultPart1(input)
     println(s"day 14 part 1: $part1")
+    val part2 = resultPart2(input)
+    println(s"day 14 part 2: $part2")
   }
 }
--- src/test/scala/com/github/tekiflo/aoc/day14/Day14Spec.scala
+++ src/test/scala/com/github/tekiflo/aoc/day14/Day14Spec.scala
@@ -6,15 +6,27 @@ import org.scalatest.matchers.must.Matchers
 class Day14Spec extends AnyFlatSpec with Matchers {
   behavior of "Day 14"
 
-  val lines: String =
-    """mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
-      |mem[8] = 11
-      |mem[7] = 101
-      |mem[8] = 0""".stripMargin
-
-  val input: Seq[Day14.Instruction] = Day14.parseInput(lines.split('\n').toSeq)
-
   it should "return the correct result for part 1 given the test input" in {
+    val lines: String =
+      """mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+        |mem[8] = 11
+        |mem[7] = 101
+        |mem[8] = 0""".stripMargin
+
+    val input: Seq[Day14.Instruction] = Day14.parseInput(lines.split('\n').toSeq)
+
     Day14.resultPart1(input) mustBe 165
+  }
+
+  it should "return the correct result for part 2 given the test input" in {
+    val lines: String =
+      """mask = 000000000000000000000000000000X1001X
+        |mem[42] = 100
+        |mask = 00000000000000000000000000000000X0XX
+        |mem[26] = 1""".stripMargin
+
+    val input: Seq[Day14.Instruction] = Day14.parseInput(lines.split('\n').toSeq)
+
+    Day14.resultPart2(input) mustBe 208
   }
 }
