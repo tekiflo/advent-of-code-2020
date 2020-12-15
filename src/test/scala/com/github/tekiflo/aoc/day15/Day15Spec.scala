40453f373586851c7940a07266f838876f7bcf3e
Florian Inchingolo <tekiflo@gmail.com>
2020-12-15 10:07:52 +0100

    day15: remove part2 test cases due to bruteforce solution

--- src/test/scala/com/github/tekiflo/aoc/day15/Day15Spec.scala
+++ src/test/scala/com/github/tekiflo/aoc/day15/Day15Spec.scala
@@ -19,18 +19,4 @@ class Day15Spec extends AnyFlatSpec with Matchers {
 
     data.foreach { case (input, expected) => Day15.resultPart1(input) mustBe expected }
   }
-
-  it should "return the correct result for part 2 given the test input" in {
-    val data = Seq(
-      ("0,3,6", 175594),
-      ("1,3,2", 2578),
-      ("2,1,3", 3544142),
-      ("1,2,3", 261214),
-      ("2,3,1", 6895259),
-      ("3,2,1", 18),
-      ("3,1,2", 362)
-    ).map { case (raw, expected) => (Day15.parseInput(raw), expected) }
-
-    data.foreach { case (input, expected) => Day15.resultPart2(input) mustBe expected }
-  }
 }
