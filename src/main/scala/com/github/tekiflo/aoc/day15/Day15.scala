cf000bed4f92ff4469748c298db6d9b15b59ddb3
Florian Inchingolo <tekiflo@gmail.com>
2020-12-15 10:06:31 +0100

    day15: add part 2 solution

--- src/main/scala/com/github/tekiflo/aoc/day15/Day15.scala
+++ src/main/scala/com/github/tekiflo/aoc/day15/Day15.scala
@@ -1,25 +1,30 @@
 package com.github.tekiflo.aoc.day15
 
-import scala.annotation.tailrec
-
 object Day15 {
-  def resultPart1(input: Seq[Int], toIndex: Int = 2020): Int = {
-    @tailrec def rec(last: Int, occ: Map[Int, Int], i: Int): Int =
-      if (i == toIndex) last
-      else rec(occ.get(last).fold(0)(i - _), occ.updated(last, i), i + 1)
-    val (cur, occ, i) = input.foldLeft((-1, Map.empty[Int, Int], 0)) {
-      case ((_, occ, i), nb) => (nb, occ.updated(nb, i), i + 1)
+  def result(input: Seq[Int], toIndex: Int): Int = {
+    val occ = input.zipWithIndex.foldLeft(Map.empty[Int, Int]) {
+      case (occ, (nb, i)) => occ.updated(nb, i + 1)
     }
-    rec(cur, occ, i)
+    val (res, _, _) = LazyList
+      .iterate((0, occ, input.size + 1)) {
+        case (nb, occ, i) => (occ.get(nb).fold(0)(i - _), occ.updated(nb, i), i + 1)
+      }
+      .dropWhile { case (_, _, i) => i != toIndex }
+      .head
+    res
   }
 
+  def resultPart1(input: Seq[Int]): Int = result(input, toIndex = 2020)
+  def resultPart2(input: Seq[Int]): Int = result(input, toIndex = 30000000)
 
-  def parseInput(raw: String): Seq[Int] = raw.map(_.toInt).toSeq
+  def parseInput(raw: String): Seq[Int] = raw.split(',').toSeq.map(_.toInt)
 
   def main(args: Array[String]): Unit = {
     val raw   = "16,12,1,0,15,7,11"
     val input = parseInput(raw)
     val part1 = resultPart1(input)
     println(s"day 15 part 1: $part1")
+    val part2 = resultPart2(input)
+    println(s"day 15 part 2: $part2")
   }
 }
